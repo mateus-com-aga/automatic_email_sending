d376ab69c4c5ef88328283f9494ca62bf93f31bc
mateus-com-aga <senxdx.farias@gmail.com>
2024-12-03 08:03:13 -0300

    First commit

---
+++ enviar_Email.py
@@ -0,0 +1,87 @@
+﻿import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os
+
+
+def remover_acentos(palavra):
+    # Dicionário de mapeamento de vogais com acento para vogais sem acento
+    mapeamento = {
+        'á': 'a', 'à': 'a', 'â': 'a', 'ã': 'a', 'ä': 'a',
+        'é': 'e', 'è': 'e', 'ê': 'e', 'ë': 'e',
+        'í': 'i', 'ì': 'i', 'î': 'i', 'ï': 'i',
+        'ó': 'o', 'ò': 'o', 'ô': 'o', 'õ': 'o', 'ö': 'o',
+        'ú': 'u', 'ù': 'u', 'û': 'u', 'ü': 'u'
+    }
+
+    # Substituir cada caractere na palavra pelo mapeamento, se existir
+    palavra_sem_acentos = ''.join(mapeamento.get(letra, letra) for letra in palavra)
+
+    return palavra_sem_acentos
+
+
+def enviarEmail(email, nome):
+    # Configurações do servidor SMTP
+    smtp_server = 'mail.tombom.com.br'
+    port = 587  # Porta para conexão com o servidor SMTP
+    sender_email = ''
+    password = ''
+
+    # Configurações do e-mail
+    receiver_email = email
+    subject = 'Fechamento Novembro'
+    body = 'Segue fechamentos em anexo.'
+
+    # Lista de nomes de arquivo dos anexos
+    file_paths = {f'convertidos/vendas_Fixa_{remover_acentos(nome.lower())}.xlsx',
+                    f'convertidos/vendas_Movel_{remover_acentos(nome.lower())}.xlsx',
+                    f'convertidos/vendas_Vada_{remover_acentos(nome.lower())}.xlsx'}
+
+    # Criando o objeto Multipart
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+
+    # Adicionando o corpo do e-mail
+    msg.attach(MIMEText(body, 'plain'))
+
+    # Loop sobre cada arquivo na lista de caminhos dos arquivos
+    for file_path in file_paths:
+        # Anexo
+        filename = os.path.basename(file_path)  # Obtém apenas o nome do arquivo
+        attachment = open(file_path, 'rb')
+
+        # Criando o objeto MIMEBase
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+        # Adicionando o anexo ao e-mail
+        msg.attach(part)
+
+    # Iniciando a conexão com o servidor SMTP
+    server = smtplib.SMTP(smtp_server, port)
+    server.starttls()
+    server.login(sender_email, password)
+
+    # Enviando o e-mail
+    text = msg.as_string()
+    server.sendmail(sender_email, receiver_email, text)
+
+    # Encerrando a conexão com o servidor SMTP
+    server.quit()
+
+
+nomes_unicos = ['nome_1']
+
+emails_unicos = ['email_1']
+print(nomes_unicos)
+
+# Iterando sobre as listas de nomes e emails
+for nome, email in zip(nomes_unicos, emails_unicos):
+    enviarEmail(email, nome)
+    print(f'Enviado com sucesso para: {nome}')
---
+++ fechamento_ftth.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+# Carrega o CSV, especificando o delimitador
+# tabela_Movel = pd.read_csv('C:\\Users\\Math\\Documents\\fechamento\\movel.csv', delimiter=';', encoding='utf-8-sig')
+tabela_Movel = pd.read_csv('/home/math/Documents/fechamento/fixa.csv', delimiter=';', encoding='utf-8-sig')
+
+# Substituir vírgulas por pontos em todas as células do DataFrame
+tabela_Movel = tabela_Movel.replace(',', '.', regex=True)
+
+def remover_acentos(palavra):
+    mapeamento = {
+        'á': 'a', 'à': 'a', 'â': 'a', 'ã': 'a', 'ä': 'a',
+        'é': 'e', 'è': 'e', 'ê': 'e', 'ë': 'e',
+        'í': 'i', 'ì': 'i', 'î': 'i', 'ï': 'i',
+        'ó': 'o', 'ò': 'o', 'ô': 'o', 'õ': 'o', 'ö': 'o',
+        'ú': 'u', 'ù': 'u', 'û': 'u', 'ü': 'u'
+    }
+    palavra_sem_acentos = ''.join(mapeamento.get(letra, letra) for letra in palavra)
+    return palavra_sem_acentos
+
+def separarExcel(executivo):
+    vendas_Executivo = tabela_Movel.loc[tabela_Movel['VENDEDOR'] == executivo]
+    nomeSemAcento = remover_acentos(executivo.lower())
+    # vendas_Executivo.to_excel(f'C:\\Users\\Math\\Documents\\fechamento\\vendas_Movel_{nomeSemAcento}.xlsx', index=False, engine='openpyxl')
+    vendas_Executivo.to_excel(f'/home/math/Documents/fechamento/convertidos/vendas_Fixa_{nomeSemAcento}.xlsx', index=False, engine='openpyxl')
+
+# Lista de nomes únicos dos executivos
+nomes_unicos = ['']
+print(nomes_unicos)
+
+# Iteração para salvar arquivos separados
+for nome in nomes_unicos:
+    separarExcel(nome)
